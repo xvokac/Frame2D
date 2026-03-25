e111c96a0b93a79468bd3ac951c1c1ff61f696fb
Miroslav Vokáč <61604479+xvokac@users.noreply.github.com>
2026-03-25 16:26:58 +0100

    Update Frame_2D.py

--- Frame_2D.py
+++ Frame_2D.py
@@ -1055,7 +1055,7 @@ class Model:
             ax.plot(xs, ys, 'r', linewidth=2)
 
         lam = self.eigenvalues[mode_index]
-        ax.set_title(fr"Eigen shape no. {mode_index+1}: $\lambda$ = {lam:.2f}")
+        ax.set_title(fr"Eigen shape no. {mode_index+1}: $\alpha_{cr}$ = {lam:.2f}")
         ax.axis("equal")
         ax.set_axis_off()
 
